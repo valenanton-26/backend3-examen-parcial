94adebb91258e0af2f74734d18541096523dd064
Valentina Anton <valuanton@gmail.com>
2022-08-24 20:28:22 -0300

    codigo base

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/valenanton-26/backend3-examen-parcial
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"clase12-examen\desafio-go-bases\internal\tickets"
+)
+
+func main() {
+	total, err := tickets.GetTotalTickets("Brazil")
+}
